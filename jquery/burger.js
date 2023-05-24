ebbecba6c92c16bb3fa0ad8f3ed62f8e855189b0
Feor <98813579+VladFeor@users.noreply.github.com>
2023-05-24 12:19:18 +0300

    Add files via upload

--- jquery/burger.js
+++ jquery/burger.js
@@ -1,27 +1,31 @@
 var toggleButton = document.querySelectorAll('#toggleButton');
 var panel = document.getElementById('panel');
+var body = document.querySelector("body");
 toggleButton.forEach(element=>{
     element.addEventListener('click', function() {
         panel.classList.toggle('open'); 
+        body.classList.toggle("noscroll");
       });
 })
 window.addEventListener("resize", function() {
   var pageWidth = window.innerWidth;
+  
   var panel = document.getElementById("panel");
   if (pageWidth <= 1050) {
     panel.classList.remove("open");
+    
   } else {
     panel.classList.add("open");
   }
 });
 var pageWidth = window.innerWidth;
 var panel = document.getElementById("panel");
-var body = document.querySelector("body");
+
 console.log(body)
 if (pageWidth <= 1050) {
   panel.classList.remove("open");
-  body.classList.add("noscroll");
+  
 } else {
   panel.classList.add("open");
-  body.classList.remove("noscroll");
+  
 }
